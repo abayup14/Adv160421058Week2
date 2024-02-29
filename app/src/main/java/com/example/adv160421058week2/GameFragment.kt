f7e87dc2fb288adf753faf95c14c02d07bd17e61
Andreas Bayu Prakasa <abayup1498@gmail.com>
2024-02-29 19:19:32 +0700

    selesai sampe sebelum kerja homework

---
+++ app/src/main/java/com/example/adv160421058week2/GameFragment.kt
@@ -0,0 +1,34 @@
+package com.example.adv160421058week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.adv160421058week2.databinding.FragmentGameBinding
+
+class GameFragment : Fragment() {
+    private lateinit var binding:FragmentGameBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
+            binding.txtTurn.text = "$playerName's Turn"
+        }
+        binding.btnBack.setOnClickListener {
+            val action = GameFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
